b80c7acf45faf7804827c1322b0f501eebedb05e
Jaakko Perttilä <jormangeud@gmail.com>
2018-09-03 20:04:54 +0300

    Bump net-p2p/rtorrent to 0.9.7 with pyroscope patches.

--- net-p2p/rtorrent/files/.get_pyroscope.bash
+++ net-p2p/rtorrent/files/.get_pyroscope.bash
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+umask 0012
+
 PWD=$(pwd)
 PYRO_DIR="ps"
 PYRO_URI="https://raw.githubusercontent.com/pyroscope/rtorrent-ps/master/patches"
 PYRO_FILES="\
+backport_0.9.6_algorithm_median.patch \
 command_pyroscope.cc \
-ps-info-pane-xb-sizes_all.patch \
-ps-item-stats-human-sizes_all.patch \
-ps-ssl_verify_host_all.patch \
-ps-throttle-steps_all.patch \
 ps-ui_pyroscope_all.patch \
+ps-view-filter-by_all.patch \
 pyroscope.patch \
 ui_pyroscope.cc \
 ui_pyroscope.h \
 ui_pyroscope.patch"
 
-mkdir $PYRO_DIR
-cd $PYRO_DIR
+mkdir -p $PYRO_DIR
+pushd $PYRO_DIR
 
 for f in $PYRO_FILES; do
     wget -q -nc $PYRO_URI/$f
 done
 
-cd $PWD
+popd
+
+patch -p0 < .ps_fix-ui_pyroscope-patch-0.9.7.patch
+
+# get master patch
+# wget https://github.com/rakshasa/rtorrent/compare/v0.9.7...master.diff -O rtorrent-0.9.7...master-for-pyroscope.diff
